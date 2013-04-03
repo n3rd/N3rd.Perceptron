747726151cb67f75d33b91165ed8f45b8fe3c5ec
Boaz <boaz@n3rd.nl>
2013-04-03 21:30:12 +0200

    initial import

---
+++ N3rd.Perceptron.App/PerceptronApp.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using N3rd.Perceptron;
+
+namespace N3rd.Perceptron.App
+{
+
+    /// <summary>
+    /// Source:
+    /// http://visualstudiomagazine.com/articles/2013/03/01/pattern-recognition-with-perceptrons.aspx
+    /// </summary>
+    class PerceptronApp
+    {
+
+        private int[][] _TrainingData = new int[][] { 
+            new int[] { 0, 1, 1, 0,
+                        1, 0, 0, 1,
+                        1, 1, 1, 1,
+                        1, 0, 0, 1,
+                        1, 0, 0, 1},
+            new int[] { 1, 1, 1, 0,
+                        1, 0, 0, 1,
+                        1, 1, 1, 0,
+                        1, 0, 0, 1,
+                        1, 1, 1, 0},
+            new int[] { 0, 1, 1, 1,
+                        1, 0, 0, 0,
+                        1, 0, 0, 0,
+                        1, 0, 0, 0,
+                        0, 1, 1, 1},
+            new int[] { 1, 1, 1, 0,
+                        1, 0, 0, 1,
+                        1, 0, 0, 1,
+                        1, 0, 0, 1,
+                        1, 1, 1, 0},
+            new int[] { 1, 1, 1, 1,
+                        1, 0, 0, 0,
+                        1, 1, 1, 0,
+                        1, 0, 0, 0,
+                        1, 1, 1, 1},
+        };
+
+        private bool[] _TrainingDataResult = new bool[] {
+            false,
+            true,
+            false,
+            false,
+            false,
+        };
+
+        private int[][] _ClassificationExamples = new int[][] {
+            new int[] { 0, 1, 1, 0,
+                        0, 0, 0, 1,
+                        1, 1, 1, 0,
+                        1, 0, 0, 1,
+                        1, 1, 1, 0},
+            new int[] { 0, 1, 1, 0,
+                        0, 0, 0, 1,
+                        1, 1, 1, 0,
+                        1, 0, 0, 1,
+                        1, 1, 0, 0},
+            new int[] { 0, 1, 1, 0,
+                        0, 0, 0, 1,
+                        1, 1, 1, 1,
+                        1, 0, 0, 1,
+                        1, 0, 0, 0},
+        };
+
+        static void Main(string[] args)
+        {
+            PerceptronApp pApp = new PerceptronApp();
+
+            pApp.Run();
+
+            Console.ReadKey();
+        }
+
+        public void Run()
+        {
+            Perceptron p = new Perceptron();
+
+            Console.WriteLine("Training data:");
+            Console.WriteLine("==============");
+            _TrainingData
+                .ToList()
+                .ForEach(d => PrintDataSet(d));
+
+            p.Learn(_TrainingData, _TrainingDataResult);
+
+            for (int i = 0; i < _ClassificationExamples.Length; i++)
+            {
+                Console.WriteLine("Classification Example (" + i + "):");
+                Console.WriteLine("===========================");
+                PrintDataSet(_ClassificationExamples[i]);
+
+                bool result = p.Classify(_ClassificationExamples[i]);
+
+                Console.WriteLine("Output (" + i + "):");
+                Console.WriteLine("===========");
+                ConsoleColor originalColor = Console.ForegroundColor;
+                Console.ForegroundColor = result ? ConsoleColor.Green : ConsoleColor.Red;
+                Console.WriteLine(result ? "Positive" : "Negative");
+                Console.ForegroundColor = originalColor;
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+
+        void PrintDataSet(int[] dataSet)
+        {
+            for (int i = 0; i < dataSet.Length; i += 4)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write(dataSet[i + j] == 1 ? "1" : " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ N3rd.Perceptron/Perceptron.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace N3rd.Perceptron
+{
+    public class Perceptron
+    {
+
+        int MaxEpochs = 1000;
+        double LearningRate = 0.075;
+        double TargetError = 0.0;
+
+        double[] Weights;
+        double Bias;
+
+        public void Learn(int[][] trainingData, bool[] trainingDataResult)
+        {
+            Weights = new double[trainingData[0].Length];
+            Bias = 0.05;
+
+            double totalError = double.MaxValue;
+
+            for (int i = 0; i < MaxEpochs && totalError > TargetError; i++)
+            {
+                for(int j=0;j<trainingData.Length; j++)
+                {
+                    int output = Classify(trainingData[j]) ? 1 : 0;
+                    int desired = trainingDataResult[j] ? 1 : 0;
+                    int delta = desired - output;
+
+                    AdjustWeights(trainingData[j], delta);
+                }
+            }
+        }
+
+        private double CalculateErrors(int[][] trainingData, bool[] trainingDataResult)
+        {
+            double sum = 0.0;
+
+            for (int i = 0; i < trainingData.Length; i++)
+            {
+                bool desired = trainingDataResult[i];
+                bool output = Classify(trainingData[i]);
+                sum += output == desired ? 1 : 0;
+            }
+
+            return 0.5 * sum;
+        }
+
+        private void AdjustWeights(int[] dataSet, int delta)
+        {
+            for(int i = 0; i < dataSet.Length; i++)
+            {
+                Weights[i] += (LearningRate * (double)delta * (double)dataSet[i]); 
+            }
+
+            Bias += (LearningRate * delta);
+        }
+
+        public bool Classify(int[] dataSet)
+        {
+            double dotProduct = 0.0;
+
+            for (int i = 0; i < Weights.Length; i++)
+            {
+                dotProduct += Weights[i] * (double)dataSet[i];
+            }
+            dotProduct += Bias;
+
+            return ActivationFunction(dotProduct);
+        }
+
+       private bool ActivationFunction(double x)
+        {
+            return x > 0.5;
+        }
+
+    }
+}
